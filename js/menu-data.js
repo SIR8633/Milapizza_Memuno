afc18c80b20ca114c820d4fc3a2bd3166d470fe1
SIR8633 <133795186+SIR8633@users.noreply.github.com>
2025-05-24 20:33:21 -0300

    Update menu-data.js

--- js/menu-data.js
+++ js/menu-data.js
@@ -4,21 +4,21 @@ const menuData = {
             "id": "mila_carne",
             "nombre": "Milapizza Clásica (Carne)",
             "descripcion": "Nuestra milanesa de ternera gigante con salsa de tomate, mozzarella y orégano.",
-            "precio": 3500,
+            "precio": 12000,
             "img": "img/menu/mila_carne.png"
         },
         {
             "id": "mila_pollo",
             "nombre": "Milapizza Clásica (Pollo)",
-            "descripcion": "Pechuga de pollo gigante empanada, con salsa, mozzarella y amor.",
-            "precio": 3300,
+            "descripcion": "Pechuga de pollo gigante empanada, con salsa, mozzarella.",
+            "precio": 12000,
             "img": "img/menu/mila_pollo.png"
         },
         {
             "id": "mila_napo",
             "nombre": "Milapizza Napolitana",
             "descripcion": "La clásica de carne o pollo, con jamón, mozzarella, tomate fresco y orégano.",
-            "precio": 3800,
+            "precio": 15000,
             "img": "img/menu/mila_napolitana.png"
         }
     ],
@@ -27,28 +27,28 @@ const menuData = {
             "id": "papas",
             "nombre": "Porción de Papas Fritas",
             "descripcion": "Crujientes y doradas, el acompañante perfecto.",
-            "precio": 1200,
+            "precio": 6000,
             "img": "img/menu/mila_papas.png"
         },
         {
             "id": "huevo",
             "nombre": "Huevo Frito",
             "descripcion": "Agregale uno (¡o dos!) a tu milapizza.",
-            "precio": 400,
+            "precio": 900,
             "img": "img/menu/huevo.jpg"
         },
         {
             "id": "salsa",
             "nombre": "Salsa Extra",
             "descripcion": "Más cheddar, barbacoa, o la que te guste.",
-            "precio": 350,
+            "precio": 300,
             "img": "img/menu/salsa.jpg"
         },
         {
             "id": "cheddar",
             "nombre": "Baño de Cheddar",
             "descripcion": "Un diluvio de queso cheddar fundido.",
-            "precio": 700,
+            "precio": 900,
             "img": "img/menu/cheddar.jpg"
         }
     ],
@@ -57,14 +57,14 @@ const menuData = {
             "id": "coca_1_5",
             "nombre": "Coca-Cola 1.5L",
             "descripcion": "Refrescante y clásica.",
-            "precio": 1500,
+            "precio": 3900,
             "img": "img/menu/coca.jpg"
         },
         {
             "id": "agua_s_gas",
             "nombre": "Agua Sin Gas 500ml",
             "descripcion": "Para hidratarse.",
-            "precio": 700,
+            "precio": 900,
             "img": "img/menu/agua.jpg"
         }
     ]
